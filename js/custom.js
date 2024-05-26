a76f783c4a763f6a732b1f06b18eb2786f35e622
bys <168626076+byung3059@users.noreply.github.com>
2024-05-26 21:44:41 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,18 @@
 $(function () {
 
+    $(window).on('scroll', function () {
+        let sct = $(window).scrollTop();
+        console.log(sct);
+        if (sct > 1) {
+            $('.header').addClass('on')
+        } else {
+            $('.header').removeClass('on')
+        }
+    })
+
     var swiper = new Swiper(".swiper", {
         loop: true,
-        slidesPerView: 2,
+        slidesPerView: 1,
         spaceBetween: 30,
         centeredSlides: true,
         navigation: {
@@ -10,5 +20,19 @@ $(function () {
             prevEl: '.swiper-button-prev',
         },
     });
+
+    $('.to_top button').on('click', function () {
+        $('html, body').animate({ scrollTop: 0, }, 300)
+    });
+
+    $(window).on('scroll', function () {
+        let sct = $(window).scrollTop();
+        console.log(sct);
+        if (sct > 900) {
+            $('.to_top').addClass('on')
+        } else {
+            $('.to_top').removeClass('on')
+        }
+    })
 
 })
